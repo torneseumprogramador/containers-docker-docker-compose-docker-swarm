6718b10633613a3cc23ca9c8131883a2791cea1e
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-05-18 07:19:06 -0300

    adicionado um rds com postgre

--- 0-ec2-docker/terraform/main.tf
+++ 0-ec2-docker/terraform/main.tf
@@ -31,6 +31,17 @@ resource "aws_security_group" "acessos_docker" {
       prefix_list_ids = null,
       security_groups: null,
       self: null
+    },
+    {
+      description      = "Liberando HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
     }
   ]
 
@@ -55,4 +66,65 @@ resource "aws_security_group" "acessos_docker" {
 
 output "aws_instance_e_ssh" {
   value = aws_instance.maquina_docker.public_dns
+}
+
+
+
+resource "aws_security_group" "acesso_postgre" {
+  name        = "acesso_postgre"
+  description = "acessos inbound traffic"
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 5432
+      to_port          = 5432
+      protocol         = "tcp"
+      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      # cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"],
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "acesso postgre"
+  }
+}
+
+
+resource "aws_db_instance" "default" {
+  engine            = "postgres"
+  engine_version    = "14.1"
+  instance_class    = "db.t4g.micro"
+  name           = "pg_desafio"
+  username       = "pg"
+  password       = "pgdesafio345"
+  allocated_storage = 5 # aloca 5 GiB
+  publicly_accessible = true # libera acesso publico
+  vpc_security_group_ids = ["${aws_security_group.acesso_postgre.id}"]
+  backup_retention_period = 0 # não criar backup periódico
+  # preferred_backup_window = "07:00-09:00" # hora que vai realizar o backup
+  deletion_protection = false # não protegido para exclusão
+  skip_final_snapshot = true # não criar snapshot - caso de o erro "Error: DB Instance FinalSnapshotIdentifier is required when a final snapshot is required"
+  # final_snapshot_identifier = true # caso de o erro "Error: DB Instance FinalSnapshotIdentifier is required when a final snapshot is required"
+  tags = {
+    Name = "meu postgre"
+  }
 }
